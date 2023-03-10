bfaf8e6860cc0f540720263baa6179876300e0b5
EvgenyGrishchenkov <smp_mzmo@mail.ru>
2023-03-10 18:13:07 +0300

    Решена необязательная задача

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person ("Раджиб", "Харрамос", 85));
-        people.add(new Person("Чандана", "Перера Кушантха Сингх", 33));
-        people.add(new Person("Фагуни", "Кевин Сильва", 29));
-        people.add(new Person("Индика", "Валлумулаге Бентхара Эскамос", 30));
+        people.add(new Person("Раджиб", "Харрамос", 85));
+        people.add(new Person("Чандана", "Перера Кушантха Сингх ", 33));
+        people.add(new Person("Фагуни", "Кевин Сильва", 35));
+        people.add(new Person("Индика", "Валлумулаге Бентхара Эскамос Фоэс", 30));
 
-        System.out.println(people);
-
-        Collections.sort(people, new ComparePeople());
-        System.out.println("Наиболее знатные люди Королевства Шри-Ланка");
-        for (int i=0; i< people.size(); i++) {
-            System.out.println((i+1) + ". " + people.get(i));
+        Collections.sort(people, new ComparePeople(3));
+                System.out.println("Наиболее знатные люди Королевства Шри-Ланка");
+        for (int i = 0; i < people.size(); i++) {
+            System.out.println((i + 1) + ". " + people.get(i));
         }
     }
 }
