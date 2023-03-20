a179fe7c8eaef7582c642a92253f65bfabe894be
EvgenyGrishchenkov <smp_mzmo@mail.ru>
2023-03-20 06:09:49 +0300

    В список людей добавлен фильтр на удаление по возрасту

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Раджиб", "Харрамос", 85));
+        people.add(new Person("Раджиб", "Харрамос", 16));
         people.add(new Person("Чандана", "Перера Кушантха Сингх ", 33));
+        people.add(new Person("Захра", "Мажди", 15));
         people.add(new Person("Фагуни", "Кевин Сильва", 35));
         people.add(new Person("Индика", "Валлумулаге Бентхара Эскамос Фоэс", 30));
+        people.add(new Person("Сима", "Сьюрадж", 17));
+
+        people.removeIf(x -> (x.getAge()) < 18);
 
         Collections.sort(people, new ComparePeople(3));
                 System.out.println("Наиболее знатные люди Королевства Шри-Ланка");
